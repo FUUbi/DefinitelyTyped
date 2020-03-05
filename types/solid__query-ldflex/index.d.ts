43ab08aa4e53e4e97e7a36b1845c824c96af3c55
Fuubi <fabriparrillo@hotmail.com>
2020-03-05 14:00:03 +0100

    add @solid/query-ldflex

---
+++ types/solid__query-ldflex/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for @solid/query-ldflex 2.10
+// Project: https://github.com/solid/query-ldflex#readme
+// Definitions by: Fuubi <https://github.com/FUUbi>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { Proxy } from 'ldflex';
+declare const data: Proxy;
+export default data;

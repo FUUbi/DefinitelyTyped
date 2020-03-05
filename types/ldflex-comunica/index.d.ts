89d8dda0c11034c750d0c2d4486c07b983e3cceb
Fuubi <fabriparrillo@hotmail.com>
2020-03-05 13:57:13 +0100

    add ldflex-comunica

---
+++ types/ldflex-comunica/index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for ldflex-comunica 3.2
+// Project: https://github.com/LDflex/LDflex-Comunica#readme
+// Definitions by: Fuubi <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default class ComunicaEngine {
+    constructor(uri: string)
+}
